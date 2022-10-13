95507cad4a94b429f6fe3f6de2db1faea2c5f9f1
Mayron Alves de Araujo <mayroonalves@gmail.com>
2022-10-12 19:37:28 -0600

    Added headless config

--- config/app.py
+++ config/app.py
@@ -3,6 +3,7 @@ from ._loadenv import *
 BROWSER = str(os.getenv("APP_BROWSER", "chrome")).lower()
 LANGUAGE = str(os.getenv("APP_LANGUAGE", "pt-br")).lower()
 SLEEP_TIME = int(os.getenv("APP_SLEEP_TIME", 5))
+HEADLESS = bool(os.getenv("APP_HEADLESS", True))
 CONTINUE_BUTTON_TEXT = {
     "pt-br": "Continuar",
     "en-br": "Continue",
