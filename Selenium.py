ab48ec7a5db2a4709a7d4b068bd6c10b7a674187
Mayron Alves de Araujo <mayroonalves@gmail.com>
2022-10-12 19:37:03 -0600

    added headless and browser choice to the .env file

--- Selenium.py
+++ Selenium.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
@@ -25,7 +26,11 @@ class Selenium():
         return Selenium.__instance
 
     def __init__(self) -> None:
-        self.driver: webdriver.Chrome or webdriver.Firefox or webdriver.Remote = None
+        self.__driver: webdriver.Chrome or webdriver.Firefox or webdriver.Remote = None
+
+    def kill() -> None:
+        Selenium.__driver = None
+        Selenium.__instance = None
 
     @staticmethod
     def getDriver() -> webdriver.Chrome or webdriver.Remote:
@@ -35,9 +40,13 @@ class Selenium():
             if config.BROWSER == 'chrome':
                 options = ChromeOptions()
             else:
+                profile = FirefoxProfile()
+                profile.set_preference('devtools.jsonview.enabled', False)
+                profile.update_preferences()
+
                 options = FirefoxOptions()
 
-            options.headless = True
+            options.headless = config.HEADLESS
             # options.noSandbox = True
             # options.disableGpu = True
             # options.disableExtensions = True
@@ -45,16 +54,20 @@ class Selenium():
             if localUse:
                 if config.BROWSER == 'chrome':
                     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-                    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+                    driver = webdriver.Chrome(
+                        executable_path='./chromedriver', options=options)
                 else:
-                    driver = webdriver.Firefox(options=options)
+                    driver = webdriver.Firefox(
+                        firefox_profile=profile,
+                        executable_path='./geckodriver', options=options)
             else:
                 if config.BROWSER == 'chrome':
                     driver = webdriver.Remote(command_executor=config.HUB_ADDRESS,
-                                          desired_capabilities=DesiredCapabilities.CHROME)
+                                              desired_capabilities=DesiredCapabilities.CHROME)
                 else:
                     driver = webdriver.Remote(command_executor=config.HUB_ADDRESS,
-                                          desired_capabilities=DesiredCapabilities.FIREFOX)
+                                              firefox_profile=profile,
+                                              desired_capabilities=DesiredCapabilities.FIREFOX)
 
             Selenium.__driver = driver
 
